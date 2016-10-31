3b24e52214725968cc626da138ad2ee67c15e450
Eva <penzeymoog@gmail.com>
2016-10-31 13:32:21 -0500

    refactor columns to be part of grid

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
 // This is where it all goes :)
+
+(window).scroll(function() {
+    var threshold = 50;
+    $("#test").html($(window).scrollTop());
+    if ($(window).scrollTop() >= threshold)
+        $('#fixed-img').addClass('fixed');
+    else
+        $('#fixed-img').removeClass('fixed');
+    var check = $("#content").height() - $("#fixed-img").height()-21;
+    if ($(window).scrollTop() >= check)
+        $('#fixed-img').addClass('bottom');
+    else
+        $('#fixed-img').removeClass('bottom');
+});
